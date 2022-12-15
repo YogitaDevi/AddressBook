36e0efd00100a756d043e32152c0ba358a240755
YogitaDevi <yogita.gry@gmail.com>
2022-12-15 15:33:16 +0530

    Add_Contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,10 +5,12 @@
         public static List<contact> Person = new List<contact>();
         public static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Address Book: ");
             AddressBook.Person.CreateContacts();
             AddressBook.Person.editContact();
             AddressBook.Person.Display();
             AddressBook.Person.DeleteContact();
+            
         }
     }
 }
---
+++ UC1_Contact/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UC1_Contact
+{
+    class Program
+    {
+        public static List<Contact> Person = new List<Contact>();
+        public static void Main(string[] args)
+        {
+            UC1_Contact.Person.CreateContacts();
+        }
+    }
+}

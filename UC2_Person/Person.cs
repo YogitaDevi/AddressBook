83b559781eae8575cb0d8c96cc7c6cf8e5ef97d8
YogitaDevi <yogita.gry@gmail.com>
2022-12-15 15:43:23 +0530

    Add_NewContact

---
+++ UC2_Person/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_Person
+{
+    public class Person
+    {
+        public static List<Contact> person = new List<Contact>();
+        public static void CreateContacts()
+        {
+            Contact contacts = new Contact();
+
+            Console.WriteLine("Enter First Name: ");
+            contacts.FName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name: ");
+            contacts.LName = Console.ReadLine();
+
+            Console.WriteLine("Enter address: ");
+            contacts.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter city: ");
+            contacts.City = Console.ReadLine();
+
+            Console.WriteLine("Enter State: ");
+            contacts.State = Console.ReadLine();
+
+            Console.WriteLine("Enter Zipcode: ");
+            contacts.Zipcode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Phone Number: ");
+            contacts.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter E-mail: ");
+            contacts.Email = Console.ReadLine();
+            person.Add(contacts);
+        }
+    }
+}
---
+++ UC2_Person/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UC2_Person
+{
+    class Program
+    {
+        public static List<Contact> Person = new List<Contact>();
+        public static void Main(string[] args)
+        {
+            UC2_Person.Person.CreateContacts();
+
+        }
+    }
+}

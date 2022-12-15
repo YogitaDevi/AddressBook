e728238c40113200edf46783896fa5e3e7296c44
YogitaDevi <yogita.gry@gmail.com>
2022-12-15 16:27:03 +0530

    [yogita] Add Multiple Contact

---
+++ UC6_RefactorAddressBook/Person.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC6_RefactorAddressBook
+{
+    public class Person
+    {
+        public static List<Contacts> person = new List<Contacts>();
+
+        public static object Remove { get; private set; }
+
+        public static void CreateContacts()
+        {
+            Contacts contacts = new Contacts();
+
+            Console.WriteLine("Enter First Name: ");
+            contacts.FName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name: ");
+            contacts.LName = Console.ReadLine();
+
+            Console.WriteLine("Enter address: ");
+            contacts.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter city: ");
+            contacts.City = Console.ReadLine();
+
+            Console.WriteLine("Enter State: ");
+            contacts.State = Console.ReadLine();
+
+            Console.WriteLine("Enter Zipcode: ");
+            contacts.Zipcode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Phone Number: ");
+            contacts.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter E-mail: ");
+            contacts.Email = Console.ReadLine();
+            person.Add(contacts);
+        }
+        public static void Display()
+        {
+            foreach (var data in person)
+            {
+                Console.WriteLine();
+                Console.WriteLine("1.FirstName: " + data.FName + "2.LastName: " + data.LName + "3.Address: " + data.Address + "4.City: " + data.City + "5.State: " + data.State + "6.Zipcode: " + data.Zipcode + "7.PhoneNumber: " + data.PhoneNumber);
+            }
+        }
+        public static void editContact()
+        {
+            Console.WriteLine("Enter the First Name: ");
+            string name = Console.ReadLine();
+            foreach (var data in person)
+            {
+                if (person.Contains(data))
+                {
+                    if (data.FName == name)
+                    {
+                        Console.WriteLine("To edit contacts enter 1.FirstName\n 2.LastName\n 3.Address\n 4.City\n 5.State\n 6.Zipcode\n 7.PhoneNumber\n");
+                        int option = Convert.ToInt32(Console.ReadLine());
+
+                        switch (option)
+                        {
+                            case 1:
+                                string fName = Console.ReadLine();
+                                data.FName = fName;
+                                break;
+
+                            case 2:
+                                string lName = Console.ReadLine();
+                                data.LName = lName;
+                                break;
+
+                            case 3:
+                                string address = Console.ReadLine();
+                                data.Address = address;
+                                break;
+
+                            case 4:
+                                string city = Console.ReadLine();
+                                data.City = city;
+                                break;
+
+                            case 5:
+                                string state = Console.ReadLine();
+                                data.State = state;
+                                break;
+
+                            case 6:
+                                int zipcode = Convert.ToInt32(Console.ReadLine());
+                                data.Zipcode = zipcode;
+                                break;
+
+                            case 7:
+                                int phoneNumber = Convert.ToInt32(Console.ReadLine());
+                                data.PhoneNumber = phoneNumber;
+                                break;
+
+                            default:
+                                Console.WriteLine("Choose valid option");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Name doesnot match");
+                    }
+                }
+            }
+        }
+        public static void DeleteContact(string name)
+        {
+            Contacts contacts = new Contacts();
+            foreach (var data in person)
+            {
+                if (data.FName == name)
+                {
+                    contacts = data;
+                }
+            }
+            person.Remove(contacts);
+        }
+        public static void AddContact()
+        {
+            Contacts contacts = new Contacts();
+            Console.WriteLine("Please confirm how much contact you want to create:");
+            int contactsCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= contactsCount; i++)
+            {
+                Console.WriteLine("Enter details for " + i + " Contact");
+
+                Console.WriteLine("Enter FirstName: ");
+                contacts.FName = Console.ReadLine();
+
+                Console.WriteLine("Enter LastName: ");
+                contacts.LName = Console.ReadLine();
+
+                Console.WriteLine("Enter Adress: ");
+                contacts.Address = Console.ReadLine();
+
+                Console.WriteLine("Enter City: ");
+                contacts.City = Console.ReadLine();
+
+                Console.WriteLine("Enter State: ");
+                contacts.State = Console.ReadLine();
+
+                Console.WriteLine("Enter Zipcode: ");
+                contacts.Zipcode = (int)Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Phone Number: ");
+                contacts.PhoneNumber = (int)Convert.ToInt64(Console.ReadLine());
+
+                Console.WriteLine("Enter EmailId: ");
+                contacts.Email = Console.ReadLine();
+
+                person.Add(contacts);
+                Console.WriteLine("Contact added Successfully....");
+            }
+        }
+    }
+}
---
+++ UC6_RefactorAddressBook/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+
+namespace UC6_RefactorAddressBook
+{
+    class Program
+    {
+        //public static List<Contacts> Person = new List<Contacts>();
+        public static void Main(string[] args)
+        {
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("Welcome to Address Book Program");
+                Console.WriteLine("Select Option:\n" +
+                    "1) Find the Create Contacts\n" +
+                    "2) Find the Display of Contacts\n" +
+                    "3) Find the edit contacts\n" +
+                    "4) Find the delete contacts\n" +
+                    "5) Find the ability to add multiple Person\n" +
+                    "6) Add multiple address to system\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                int count = 0;
+                switch (option)
+                {
+                    case 1:
+                        Person.CreateContacts();
+                        break;
+
+                    case 2:
+                        Person.Display();
+                        break;
+
+                    case 3:
+                        Person.editContact();
+                        break;
+
+                    case 4:
+                        string number = Console.ReadLine();
+                        Person.DeleteContact(number);
+                        break;
+
+                    case 5:
+                        Person.AddContact();
+                        break;
+
+                    case 6:
+                        int addCount = Convert.ToInt32(Console.ReadLine());
+
+                        Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+                        for (int i = 1; i <= addCount; i++)
+                        {
+                            count++;
+                            List<Person> newAdd = new List<Person>();
+                            dictionary.Add(i, "New Dictionary");
+                        }
+                        Console.WriteLine(count + " Address Book Created..");
+                        Person.AddContact();
+                        break;
+                    default:
+                        Console.WriteLine("Choose valid option");
+                        break;
+                }
+            }
+        }
+    }
+}

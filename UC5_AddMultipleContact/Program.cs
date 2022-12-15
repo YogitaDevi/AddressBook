3796d27b1602cfc7920096c01b87836e42c70fe6
YogitaDevi <yogita.gry@gmail.com>
2022-12-15 16:17:10 +0530

    [yogita] Add Multiple Contact

---
+++ UC5_AddMultipleContact/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace UC5_AddMultipleContact
+{
+    class Program
+    {
+        //public static List<Contacts> Person = new List<Contacts>();
+        public static void Main(string[] args)
+        {
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("Welcome to Address Book Program");
+                Console.WriteLine("Select Option:\n" +
+                    "1) Find the Create Contacts\n" +
+                    "2) Find the Display of Contacts\n" +
+                    "3) Find the edit contacts\n" +
+                    "4) Find the delete contacts\n" +
+                    "5) Find the ability to add multiple Person\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Person.CreateContacts();
+                        break;
+
+                    case 2:
+                        Person.Display();
+                        break;
+
+                    case 3:
+                        Person.editContact();
+                        break;
+
+                    case 4:
+                        string number = Console.ReadLine();
+                        Person.DeleteContact(number);
+                        break;
+
+                    case 5:
+                        Person.AddContact();
+                        break;
+                    default:
+                        Console.WriteLine("Choose valid option");
+                        break;
+                }
+            }
+        }
+    }
+}
